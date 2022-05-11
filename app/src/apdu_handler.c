e0bc463a327e5dc8984c6c794b5d4d549c20ec0e
ftheirs <fernando@zondax.ch>
2022-05-11 12:40:17 -0300

    implement apdu handler: get version and get address OK

--- app/src/apdu_handler.c
+++ app/src/apdu_handler.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
-*   (c) 2018, 2019 Zondax GmbH
+*   (c) 2018, 2022 Zondax GmbH
 *   (c) 2016 Ledger
 *
 *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,148 +23,238 @@
 #include <ux.h>
 
 #include "view.h"
-// #include "actions.h"
-// #include "tx.h"
-// #include "addr.h"
-// #include "crypto.h"
+#include "actions.h"
+#include "tx.h"
+#include "addr.h"
+#include "crypto.h"
 #include "coin.h"
 #include "zxmacros.h"
 
 #include "apdu_protocol_defines.h"
+#include "algo_tx.h"
+#include "algo_keys.h"
+#include "command_handler.h"
+
+#include "crypto.h"
+#include "algo_addr.h"
+
 static bool tx_initialized = false;
 
+#if defined(TARGET_NANOX) || defined(TARGET_NANOS2)
+#define TNX_BUFFER_SIZE 2048
+#else
+#define TNX_BUFFER_SIZE 900
+#endif
 
-// From main.c ---> move here
+uint8_t msgpack_buf[TNX_BUFFER_SIZE];
+unsigned int msgpack_next_off;
+
+struct pubkey_s public_key;
+txn_t current_txn;
+
+__Z_INLINE bool process_chunk(__Z_UNUSED volatile uint32_t *tx, uint32_t rx) {
+    const uint8_t payloadType = G_io_apdu_buffer[OFFSET_PAYLOAD_TYPE];
+    if (rx < OFFSET_DATA) {
+        THROW(APDU_CODE_WRONG_LENGTH);
+    }
+
+    uint32_t added;
+    switch (payloadType) {
+        case P1_INIT:
+            tx_initialize();
+            tx_reset();
+            extractHDPath(rx, OFFSET_DATA);
+            // check here account id (parse_input_for_get_public_key_command)
+            // parse_input_for_get_public_key_command(G_io_apdu_buffer, rx, &account_id);
+            tx_initialized = true;
+            return false;
+        case P1_ADD:
+            if (!tx_initialized) {
+                THROW(APDU_CODE_TX_NOT_INITIALIZED);
+            }
+            added = tx_append(&(G_io_apdu_buffer[OFFSET_DATA]), rx - OFFSET_DATA);
+            if (added != rx - OFFSET_DATA) {
+                tx_initialized = false;
+                THROW(APDU_CODE_OUTPUT_BUFFER_TOO_SMALL);
+            }
+            return false;
+        case P1_LAST:
+            if (!tx_initialized) {
+                THROW(APDU_CODE_TX_NOT_INITIALIZED);
+            }
+            added = tx_append(&(G_io_apdu_buffer[OFFSET_DATA]), rx - OFFSET_DATA);
+            tx_initialized = false;
+            if (added != rx - OFFSET_DATA) {
+                tx_initialized = false;
+                THROW(APDU_CODE_OUTPUT_BUFFER_TOO_SMALL);
+            }
+            tx_initialized = false;
+            return true;
+    }
+
+    THROW(APDU_CODE_INVALIDP1P2);
+}
+
+// From main.c ---> moved here
+static void copy_and_advance(void *dst, uint8_t **p, size_t len)
+{
+    MEMMOVE(dst, *p, len);
+    *p += len;
+}
+
 void handle_sign_payment(uint8_t ins)
 {
+    uint8_t *p;
 
+    MEMZERO(&current_txn, sizeof(current_txn));
+
+    if (ins == INS_SIGN_PAYMENT_V2) {
+        p = &G_io_apdu_buffer[2];
+    } else {
+        p = &G_io_apdu_buffer[OFFSET_DATA];
+    }
+
+    _Static_assert(sizeof(G_io_apdu_buffer) - OFFSET_DATA >= 192, "assert");
+
+    current_txn.type = PAYMENT;
+    copy_and_advance( current_txn.sender,           &p, 32);
+    copy_and_advance(&current_txn.fee,              &p, 8);
+    copy_and_advance(&current_txn.firstValid,       &p, 8);
+    copy_and_advance(&current_txn.lastValid,        &p, 8);
+    copy_and_advance( current_txn.genesisID,        &p, 32);
+    copy_and_advance( current_txn.genesisHash,      &p, 32);
+    copy_and_advance( current_txn.payment.receiver, &p, 32);
+    copy_and_advance(&current_txn.payment.amount,   &p, 8);
+    copy_and_advance( current_txn.payment.close,    &p, 32);
+
+    // ui_txn();
+    // view_review_init(tx_getItem, tx_getNumItems, txn_approve);
+    // view_review_show();
+    // *flags |= IO_ASYNCH_REPLY;
 }
 
 static void handle_sign_keyreg(uint8_t ins)
 {
+    uint8_t *p;
 
+    MEMZERO(&current_txn, sizeof(current_txn));
+
+    if (ins == INS_SIGN_KEYREG_V2) {
+        p = &G_io_apdu_buffer[2];
+    } else {
+        p = &G_io_apdu_buffer[OFFSET_DATA];
+    }
+
+    _Static_assert(sizeof(G_io_apdu_buffer) - OFFSET_DATA >= 184, "assert");
+
+    current_txn.type = KEYREG;
+    copy_and_advance( current_txn.sender,        &p, 32);
+    copy_and_advance(&current_txn.fee,           &p, 8);
+    copy_and_advance(&current_txn.firstValid,    &p, 8);
+    copy_and_advance(&current_txn.lastValid,     &p, 8);
+    copy_and_advance( current_txn.genesisID,     &p, 32);
+    copy_and_advance( current_txn.genesisHash,   &p, 32);
+    copy_and_advance( current_txn.keyreg.votepk, &p, 32);
+    copy_and_advance( current_txn.keyreg.vrfpk,  &p, 32);
+    copy_and_advance( current_txn.keyreg.sprfkey,  &p, 64);
+
+    // ui_txn();
 }
 
-static int handle_sign_msgpack(volatile unsigned int rx, volatile unsigned int *tx)
+__Z_INLINE zxerr_t handle_sign_msgpack(volatile uint32_t *flags, volatile uint32_t *tx, uint32_t rx)
 {
- return 0;
-}
-static int handle_get_public_key(volatile unsigned int rx, volatile unsigned int *tx)
-{
+    if (!process_chunk(tx, rx)) {
+        THROW(APDU_CODE_OK);
+    }
+
+    char *error_msg;
+    int error;
+
+    //
+    error = parse_input_for_msgpack_command(G_io_apdu_buffer, rx, msgpack_buf, TNX_BUFFER_SIZE, &msgpack_next_off, &current_txn, &error_msg);
+    if (error) {
+        return error;
+    }
+
+    // tx_decode(current_txn_buffer, *current_txn_buffer_offset, &current_txn);
+
+    if (error_msg != NULL)
+    {
+        int errlen = strlen(error_msg);
+        MEMZERO(G_io_apdu_buffer, 65);
+        memmove(&G_io_apdu_buffer[65], error_msg, errlen);
+        *tx = 65 + errlen;
+    } else {
+        // we get here when all the data was received, otherwise an exception is thrown
+        // ui_txn();
+    }
     return 0;
 }
 
-// void extractHDPath(uint32_t rx, uint32_t offset) {
-//     tx_initialized = false;
+__Z_INLINE void handle_get_public_key(volatile uint32_t *flags, volatile uint32_t *tx, uint32_t rx)
+{
+    uint32_t account_id = 0;
+    const uint8_t requireConfirmation = G_io_apdu_buffer[OFFSET_P1];
 
-//     if ((rx - offset) < sizeof(uint32_t) * HDPATH_LEN_DEFAULT) {
-//         THROW(APDU_CODE_WRONG_LENGTH);
-//     }
+    zxerr_t err = parse_input_for_get_public_key_command(G_io_apdu_buffer, rx, &account_id);
+    if (err != zxerr_ok) {
+        THROW(APDU_CODE_UNKNOWN);
+    }
 
-//     MEMCPY(hdPath, G_io_apdu_buffer + offset, sizeof(uint32_t) * HDPATH_LEN_DEFAULT);
+    uint16_t bufferLen = IO_APDU_BUFFER_SIZE - 2;
+    MEMZERO(G_io_apdu_buffer, bufferLen);
 
-//     const bool mainnet = hdPath[0] == HDPATH_0_DEFAULT &&
-//                          hdPath[1] == HDPATH_1_DEFAULT;
+    err = fetch_public_key(account_id, &public_key);
+    if (err != zxerr_ok) {
+        THROW(APDU_CODE_UNKNOWN);
+    }
+    MEMMOVE(G_io_apdu_buffer, public_key.data, ALGORAND_PUBLIC_KEY_SIZE);
 
-//     const bool testnet = hdPath[0] == HDPATH_0_TESTNET &&
-//                          hdPath[1] == HDPATH_1_TESTNET;
+    // convert pk ---> address
+    const uint8_t addrLen = convert_to_public_address(G_io_apdu_buffer, G_io_apdu_buffer+PK_LEN_25519);
+    action_addrResponseLen = PK_LEN_25519 + addrLen;
 
-//     if (!mainnet && !testnet) {
-//         THROW(APDU_CODE_DATA_INVALID);
-//     }
-// }
+    if (requireConfirmation) {
+        view_review_init(addr_getItem, addr_getNumItems, app_reply_address);
+        view_review_show();
+        *flags |= IO_ASYNCH_REPLY;
+        return;
+    }
 
-// bool process_chunk(__Z_UNUSED volatile uint32_t *tx, uint32_t rx) {
+    *tx = action_addrResponseLen;
+    THROW(APDU_CODE_OK);
+}
 
-//     const uint8_t payloadType = G_io_apdu_buffer[OFFSET_PAYLOAD_TYPE];
+__Z_INLINE void handle_getversion(volatile uint32_t *flags, volatile uint32_t *tx)
+{
+    G_io_apdu_buffer[0] = 0;
 
-//     if (G_io_apdu_buffer[OFFSET_P2] != 0) {
-//         THROW(APDU_CODE_INVALIDP1P2);
-//     }
+#if defined(APP_TESTING)
+    G_io_apdu_buffer[0] = 0x01;
+#endif
 
-//     if (rx < OFFSET_DATA) {
-//         THROW(APDU_CODE_WRONG_LENGTH);
-//     }
+    G_io_apdu_buffer[1] = (LEDGER_MAJOR_VERSION >> 8) & 0xFF;
+    G_io_apdu_buffer[2] = (LEDGER_MAJOR_VERSION >> 0) & 0xFF;
 
-//     uint32_t added;
-//     switch (payloadType) {
-//         case P1_INIT:
-//             tx_initialize();
-//             tx_reset();
-//             extractHDPath(rx, OFFSET_DATA);
-//             tx_initialized = true;
-//             return false;
-//         case P1_ADD:
-//             if (!tx_initialized) {
-//                 THROW(APDU_CODE_TX_NOT_INITIALIZED);
-//             }
-//             added = tx_append(&(G_io_apdu_buffer[OFFSET_DATA]), rx - OFFSET_DATA);
-//             if (added != rx - OFFSET_DATA) {
-//                 tx_initialized = false;
-//                 THROW(APDU_CODE_OUTPUT_BUFFER_TOO_SMALL);
-//             }
-//             return false;
-//         case P1_LAST:
-//             if (!tx_initialized) {
-//                 THROW(APDU_CODE_TX_NOT_INITIALIZED);
-//             }
-//             added = tx_append(&(G_io_apdu_buffer[OFFSET_DATA]), rx - OFFSET_DATA);
-//             tx_initialized = false;
-//             if (added != rx - OFFSET_DATA) {
-//                 THROW(APDU_CODE_OUTPUT_BUFFER_TOO_SMALL);
-//             }
-//             return true;
-//     }
+    G_io_apdu_buffer[3] = (LEDGER_MINOR_VERSION >> 8) & 0xFF;
+    G_io_apdu_buffer[4] = (LEDGER_MINOR_VERSION >> 0) & 0xFF;
 
-//     THROW(APDU_CODE_INVALIDP1P2);
-// }
+    G_io_apdu_buffer[5] = (LEDGER_PATCH_VERSION >> 8) & 0xFF;
+    G_io_apdu_buffer[6] = (LEDGER_PATCH_VERSION >> 0) & 0xFF;
 
-// __Z_INLINE void handleGetAddr(volatile uint32_t *flags, volatile uint32_t *tx, uint32_t rx) {
-//     extractHDPath(rx, OFFSET_DATA);
+    G_io_apdu_buffer[7] = !IS_UX_ALLOWED;
 
-//     uint8_t requireConfirmation = G_io_apdu_buffer[OFFSET_P1];
+    G_io_apdu_buffer[8] = (TARGET_ID >> 24) & 0xFF;
+    G_io_apdu_buffer[9] = (TARGET_ID >> 16) & 0xFF;
+    G_io_apdu_buffer[10] = (TARGET_ID >> 8) & 0xFF;
+    G_io_apdu_buffer[11] = (TARGET_ID >> 0) & 0xFF;
 
-//     zxerr_t zxerr = app_fill_address();
-//     if (zxerr != zxerr_ok) {
-//         *tx = 0;
-//         THROW(APDU_CODE_DATA_INVALID);
-//     }
-//     if (requireConfirmation) {
-//         view_review_init(addr_getItem, addr_getNumItems, app_reply_address);
-//         view_review_show();
-//         *flags |= IO_ASYNCH_REPLY;
-//         return;
-//     }
-//     *tx = action_addrResponseLen;
-//     THROW(APDU_CODE_OK);
-// }
-
-// __Z_INLINE void handleSign(volatile uint32_t *flags, volatile uint32_t *tx, uint32_t rx) {
-//     if (!process_chunk(tx, rx)) {
-//         THROW(APDU_CODE_OK);
-//     }
-
-//     CHECK_APP_CANARY()
-
-//     const char *error_msg = tx_parse();
-//     CHECK_APP_CANARY()
-
-//     if (error_msg != NULL) {
-//         int error_msg_length = strlen(error_msg);
-//         MEMCPY(G_io_apdu_buffer, error_msg, error_msg_length);
-//         *tx += (error_msg_length);
-//         THROW(APDU_CODE_DATA_INVALID);
-//     }
-
-//     CHECK_APP_CANARY()
-//     view_review_init(tx_getItem, tx_getNumItems, app_sign);
-//     view_review_show();
-//     *flags |= IO_ASYNCH_REPLY;
-// }
+    *tx += 12;
+    THROW(APDU_CODE_OK);
+}
 
 void handleApdu(volatile uint32_t *flags, volatile uint32_t *tx, uint32_t rx) {
     uint16_t sw = 0;
-    int error;
 
     BEGIN_TRY
     {
@@ -179,7 +269,6 @@ void handleApdu(volatile uint32_t *flags, volatile uint32_t *tx, uint32_t rx) {
             }
 
             const uint8_t ins = G_io_apdu_buffer[OFFSET_INS];
-            // switch (G_io_apdu_buffer[OFFSET_INS]) {
             switch (ins) {
                 case INS_SIGN_PAYMENT_V2:
                 case INS_SIGN_PAYMENT_V3:
@@ -189,70 +278,33 @@ void handleApdu(volatile uint32_t *flags, volatile uint32_t *tx, uint32_t rx) {
 
                 case INS_SIGN_KEYREG_V2:
                 case INS_SIGN_KEYREG_V3:
+                    if( os_global_pin_is_validated() != BOLOS_UX_OK ) {
+                        THROW(APDU_CODE_COMMAND_NOT_ALLOWED);
+                    }
                     handle_sign_keyreg(ins);
                     *flags |= IO_ASYNCH_REPLY;
                     break;
 
                 case INS_SIGN_MSGPACK:
-                    error = handle_sign_msgpack(rx, tx);
-                    if (error) {
-                        THROW(error);
+                    if (os_global_pin_is_validated() != BOLOS_UX_OK) {
+                        THROW(APDU_CODE_COMMAND_NOT_ALLOWED);
                     }
-                    if (tx != 0) {
-                        THROW(APDU_CODE_OK);
-                    } else {
-                        *flags |= IO_ASYNCH_REPLY;
-                    }
+                    handle_sign_msgpack(flags, tx, rx);
                     break;
 
-                case INS_GET_PUBLIC_KEY:
-                    error = handle_get_public_key(rx, tx);
-                    if (error) {
-                        THROW(error);
+                case INS_GET_PUBLIC_KEY: {
+                    if( os_global_pin_is_validated() != BOLOS_UX_OK ) {
+                        THROW(APDU_CODE_COMMAND_NOT_ALLOWED);
                     }
-                    if (tx != 0) {
-                        THROW(APDU_CODE_OK);
-                    } else {
-                        *flags |= IO_ASYNCH_REPLY;
-                    }
+                    handle_get_public_key(flags, tx, rx);
                     break;
+                }
 
-//                 case INS_GET_VERSION: {
-// #ifdef TESTING_ENABLED
-//                     G_io_apdu_buffer[0] = 0xFF;
-// #else
-//                     G_io_apdu_buffer[0] = 0;
-// #endif
-//                     G_io_apdu_buffer[1] = LEDGER_MAJOR_VERSION;
-//                     G_io_apdu_buffer[2] = LEDGER_MINOR_VERSION;
-//                     G_io_apdu_buffer[3] = LEDGER_PATCH_VERSION;
-//                     G_io_apdu_buffer[4] = !IS_UX_ALLOWED;
-
-//                     G_io_apdu_buffer[5] = (TARGET_ID >> 24) & 0xFF;
-//                     G_io_apdu_buffer[6] = (TARGET_ID >> 16) & 0xFF;
-//                     G_io_apdu_buffer[7] = (TARGET_ID >> 8) & 0xFF;
-//                     G_io_apdu_buffer[8] = (TARGET_ID >> 0) & 0xFF;
-
-//                     *tx += 9;
-//                     THROW(APDU_CODE_OK);
-//                     break;
-//                 }
-
-//                 case INS_GET_ADDR_SECP256K1: {
-//                     if (os_global_pin_is_validated() != BOLOS_UX_OK) {
-//                         THROW(APDU_CODE_COMMAND_NOT_ALLOWED);
-//                     }
-//                     handleGetAddr(flags, tx, rx);
-//                     break;
-//                 }
-
-//                 case INS_SIGN_SECP256K1: {
-//                     if (os_global_pin_is_validated() != BOLOS_UX_OK) {
-//                         THROW(APDU_CODE_COMMAND_NOT_ALLOWED);
-//                     }
-//                     handleSign(flags, tx, rx);
-//                     break;
-//                 }
+                case INS_GET_VERSION: {
+                    handle_getversion(flags, tx);
+                    THROW(APDU_CODE_OK);
+                    break;
+                }
 
                 default:
                     THROW(APDU_CODE_INS_NOT_SUPPORTED);
