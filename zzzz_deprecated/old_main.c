435b5971ad95e573f00df7d34d27a082145c6eaf
ftheirs <fernando@zondax.ch>
2022-05-11 12:16:42 -0300

    move old main file

--- zzzz_deprecated/old_main.c
+++ zzzz_deprecated/old_main.c
@@ -11,36 +11,36 @@
 #include "apdu_protocol_defines.h"
 // #include "ui_txn.h"
 
-unsigned char G_io_seproxyhal_spi_buffer[IO_SEPROXYHAL_BUFFER_SIZE_B];
+// unsigned char G_io_seproxyhal_spi_buffer[IO_SEPROXYHAL_BUFFER_SIZE_B];
 
 /* The transaction that we might ask the user to approve. */
-txn_t current_txn;
+// txn_t current_txn;
 
 /* A buffer for collecting msgpack-encoded transaction via APDUs,
  * as well as for msgpack-encoding transaction prior to signing.
  */
-#if defined(TARGET_NANOX) || defined(TARGET_NANOS2)
-#define TNX_BUFFER_SIZE 2048
-#else
-#define TNX_BUFFER_SIZE 900
-#endif
+// #if defined(TARGET_NANOX) || defined(TARGET_NANOS2)
+// #define TNX_BUFFER_SIZE 2048
+// #else
+// #define TNX_BUFFER_SIZE 900
+// #endif
 
-uint8_t msgpack_buf[TNX_BUFFER_SIZE];
-unsigned int msgpack_next_off;
+// uint8_t msgpack_buf[TNX_BUFFER_SIZE];
+// unsigned int msgpack_next_off;
 
-struct pubkey_s public_key;
+// struct pubkey_s public_key;
 
-void user_approval_denied(void)
-{
-  G_io_apdu_buffer[0] = 0x69;
-  G_io_apdu_buffer[1] = 0x85;
+// void user_approval_denied(void)
+// {
+//   G_io_apdu_buffer[0] = 0x69;
+//   G_io_apdu_buffer[1] = 0x85;
 
-  // Send back the response, do not restart the event loop
-  io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 2);
+//   // Send back the response, do not restart the event loop
+//   io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 2);
 
-  // Display back the original UX
-  ui_idle();
-}
+//   // Display back the original UX
+//   ui_idle();
+// }
 
 // static void copy_and_advance(void *dst, uint8_t **p, size_t len)
 // {
@@ -68,7 +68,7 @@ void user_approval_denied(void)
 
 //   _Static_assert(sizeof(G_io_apdu_buffer) - OFFSET_CDATA >= 192, "assert");
 
-//   current_txn.type = PAYMENT;
+  // current_txn.type = PAYMENT;
 //   copy_and_advance( current_txn.sender,           &p, 32);
 //   copy_and_advance(&current_txn.fee,              &p, 8);
 //   copy_and_advance(&current_txn.firstValid,       &p, 8);
@@ -277,81 +277,81 @@ void user_approval_denied(void)
 //   return;
 // }
 
-void
-io_seproxyhal_display(const bagl_element_t *element)
-{
-  io_seproxyhal_display_default((bagl_element_t *)element);
-}
+// void
+// io_seproxyhal_display(const bagl_element_t *element)
+// {
+//   io_seproxyhal_display_default((bagl_element_t *)element);
+// }
 
-unsigned char io_event(unsigned char channel) {
-  UNUSED(channel);
-  // nothing done with the event, throw an error on the transport layer if
-  // needed
+// unsigned char io_event(unsigned char channel) {
+//   UNUSED(channel);
+//   // nothing done with the event, throw an error on the transport layer if
+//   // needed
 
-  // can't have more than one tag in the reply, not supported yet.
-  switch (G_io_seproxyhal_spi_buffer[0]) {
-  case SEPROXYHAL_TAG_FINGER_EVENT:
-    UX_FINGER_EVENT(G_io_seproxyhal_spi_buffer);
-    break;
+//   // can't have more than one tag in the reply, not supported yet.
+//   switch (G_io_seproxyhal_spi_buffer[0]) {
+//   case SEPROXYHAL_TAG_FINGER_EVENT:
+//     UX_FINGER_EVENT(G_io_seproxyhal_spi_buffer);
+//     break;
 
-  case SEPROXYHAL_TAG_BUTTON_PUSH_EVENT:
-    UX_BUTTON_PUSH_EVENT(G_io_seproxyhal_spi_buffer);
-    break;
+//   case SEPROXYHAL_TAG_BUTTON_PUSH_EVENT:
+//     UX_BUTTON_PUSH_EVENT(G_io_seproxyhal_spi_buffer);
+//     break;
 
-  case SEPROXYHAL_TAG_STATUS_EVENT:
-    if (G_io_apdu_media == IO_APDU_MEDIA_USB_HID && !(U4BE(G_io_seproxyhal_spi_buffer, 3) & SEPROXYHAL_TAG_STATUS_EVENT_FLAG_USB_POWERED)) {
-      THROW(EXCEPTION_IO_RESET);
-    }
-    // no break is intentional
-  default:
-    UX_DEFAULT_EVENT();
-    break;
+//   case SEPROXYHAL_TAG_STATUS_EVENT:
+//     if (G_io_apdu_media == IO_APDU_MEDIA_USB_HID && !(U4BE(G_io_seproxyhal_spi_buffer, 3) & SEPROXYHAL_TAG_STATUS_EVENT_FLAG_USB_POWERED)) {
+//       THROW(EXCEPTION_IO_RESET);
+//     }
+//     // no break is intentional
+//   default:
+//     UX_DEFAULT_EVENT();
+//     break;
 
-  case SEPROXYHAL_TAG_DISPLAY_PROCESSED_EVENT:
-    UX_DISPLAYED_EVENT({});
-    break;
+//   case SEPROXYHAL_TAG_DISPLAY_PROCESSED_EVENT:
+//     UX_DISPLAYED_EVENT({});
+//     break;
 
-  case SEPROXYHAL_TAG_TICKER_EVENT:
-    UX_TICKER_EVENT(G_io_seproxyhal_spi_buffer,
-    {
-    });
-    break;
-  }
+//   case SEPROXYHAL_TAG_TICKER_EVENT:
+//     UX_TICKER_EVENT(G_io_seproxyhal_spi_buffer,
+//     {
+//     });
+//     break;
+//   }
 
-  // close the event if not done previously (by a display or whatever)
-  if (!io_seproxyhal_spi_is_status_sent()) {
-    io_seproxyhal_general_status();
-  }
+//   // close the event if not done previously (by a display or whatever)
+//   if (!io_seproxyhal_spi_is_status_sent()) {
+//     io_seproxyhal_general_status();
+//   }
 
-  // command has been processed, DO NOT reset the current APDU transport
-  return 1;
-}
+//   // command has been processed, DO NOT reset the current APDU transport
+//   return 1;
+// }
 
-unsigned short
-io_exchange_al(unsigned char channel, unsigned short tx_len)
-{
-  switch (channel & ~(IO_FLAGS)) {
-  case CHANNEL_KEYBOARD:
-    break;
+// unsigned short
+// io_exchange_al(unsigned char channel, unsigned short tx_len)
+// {
+//   switch (channel & ~(IO_FLAGS)) {
+//   case CHANNEL_KEYBOARD:
+//     break;
 
-  // multiplexed io exchange over a SPI channel and TLV encapsulated protocol
-  case CHANNEL_SPI:
-    if (tx_len) {
-      io_seproxyhal_spi_send(G_io_apdu_buffer, tx_len);
+//   // multiplexed io exchange over a SPI channel and TLV encapsulated protocol
+//   case CHANNEL_SPI:
+//     if (tx_len) {
+//       io_seproxyhal_spi_send(G_io_apdu_buffer, tx_len);
 
-      if (channel & IO_RESET_AFTER_REPLIED) {
-        reset();
-      }
-      return 0; // nothing received from the master so far (it's a tx transaction)
-    } else {
-      return io_seproxyhal_spi_recv(G_io_apdu_buffer, sizeof(G_io_apdu_buffer), 0);
-    }
+//       if (channel & IO_RESET_AFTER_REPLIED) {
+//         reset();
+//       }
+//       return 0; // nothing received from the master so far (it's a tx transaction)
+//     } else {
+//       return io_seproxyhal_spi_recv(G_io_apdu_buffer, sizeof(G_io_apdu_buffer), 0);
+//     }
 
-  default:
-    THROW(INVALID_PARAMETER);
-  }
-  return 0;
-}
+//   default:
+//     THROW(INVALID_PARAMETER);
+//   }
+//   return 0;
+// }
 
 // __attribute__((section(".boot")))
 // int
