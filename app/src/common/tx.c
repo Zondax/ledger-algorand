92e35b25c600bc8545ca69db89f143d04c6ccd09
0xPxt <raventosaleix@gmail.com>
2025-02-07 15:11:49 +0100

    Adapt to std Makefile

--- app/src/common/tx.c
+++ app/src/common/tx.c
@@ -17,7 +17,7 @@
 #include "tx.h"
 #include "apdu_codes.h"
 #include "buffering.h"
-#include "parser.h"
+#include "common/parser.h"
 #include <string.h>
 #include "zxmacros.h"
 #include "zxformat.h"
